255d6d2d77c89d6b002268605fb65a89a947eec0
SOUMYA RANJAN PATRA <1103775@shyamgroup.com>
2025-08-06 14:53:17 +0530

    port changes

--- app.py
+++ app.py
@@ -77,5 +77,16 @@ def predict():
 
     return render_template("index.html", prediction=prediction)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
